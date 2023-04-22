aa8b895e3c87476d8602f751903e00276298c380
Martin Levesque <levesque.martin@gmail.com>
2023-04-22 10:27:46 -0400

    wip

---
+++ app.py
@@ -0,0 +1,17 @@
+
+# Data model
+
+## rules
+### - pattern (string)
+### - category (string)
+
+## spendings
+### - date (datetime)
+### - amount (float)
+### - category (string)
+
+### indexes: (spendings.date), (spendings.date, spendings.category)
+
+# https://www.highcharts.com/blog/download/
+
+print('yolo')
